dec8184801cfb91cdeee1dcc1e453c06a20b30be
rezasdsju <rezaul2002104@gmail.com>
2026-07-14 07:38:12 +0600

    The model 'Product' has been created in models.py and been registered in admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # from . models import Category, Product, UserProfile, Order, OrderItem
-from . models import Category
+from . models import Category, Product
 
 # Register your models here.
 
@@ -8,17 +8,20 @@ from . models import Category
 
 admin.site.register(Category)
 
-# admin.site.register(Product)
-# admin.site.register(UserProfile)
-# admin.site.register(Order)
-# admin.site.register(OrderItem)
+admin.site.register(Product)
 
 
 
+#########++ Viewing tabulated format in DataBase
 '''
-#Viewing tabulated format in DataBase
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
     #list_display = ['id','name', 'slug']
+'''
+'''
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    #list_display = ['id', 'category', 'name', 'description', 'price', 'image', 'created_at']
+    list_display = ['category', 'name', 'description', 'price', 'image', 'created_at']
 '''
