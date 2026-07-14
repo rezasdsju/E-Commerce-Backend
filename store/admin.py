3f97f504c58e9438a3244ae62be8b1ab83a1a915
rezasdsju <rezaul2002104@gmail.com>
2026-07-14 13:09:01 +0600

    Three models 'UserProfile, Order, OrderItem' has been created and registered

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # from . models import Category, Product, UserProfile, Order, OrderItem
-from . models import Category, Product
+from . models import Category, Product, UserProfile, Order, OrderItem
 
 # Register your models here.
 
@@ -10,7 +10,10 @@ admin.site.register(Category)
 
 admin.site.register(Product)
 
+admin.site.register(UserProfile)
+admin.site.register(Order)
 
+admin.site.register(OrderItem)
 
 #########++ Viewing tabulated format in DataBase
 '''
@@ -25,3 +28,19 @@ class ProductAdmin(admin.ModelAdmin):
     #list_display = ['id', 'category', 'name', 'description', 'price', 'image', 'created_at']
     list_display = ['category', 'name', 'description', 'price', 'image', 'created_at']
 '''
+'''
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'phone', 'address']
+    list_display = ['id', 'user', 'phone', 'address']'''
+    
+'''    
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'created_at', 'total_amount']'''
+    
+'''    
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    #list_display = ['id', 'order', 'product', 'quantity', 'price']
+    #list_display = ['order', 'product', 'quantity', 'price']'''
--- store/models.py
+++ store/models.py
@@ -20,4 +20,37 @@ class Product(models.Model):
        
     def __str__(self):
         return self.name
- 
+    
+
+
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    #A OneToOneField in Django creates a relationship where one record in a model links to exactly one record in another model
+    phone = models.CharField(max_length=15, blank=True)
+    address = models.CharField(blank=True)
+    # bio = models.TextField(blank=True)
+    # avatar = models.URLField(blank=True)
+    
+    def __str__(self):
+        return self.user.username
+    
+    
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  
+    #models.ForeignKey is used to define a many-to-one relationship between database tables
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return f"Order {self.id} by {self.user.username}"
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
